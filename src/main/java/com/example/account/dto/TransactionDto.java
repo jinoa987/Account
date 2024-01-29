b2c9b41be404a63a4b1489e5ff3a374e6b16603e
AnJiwon941205 <77887291+AnJi-won@users.noreply.github.com>
2024-01-29 21:58:58 +0900

    잔액 사용 API - useBalance success 테스트 코드 작성

---
+++ src/main/java/com/example/account/dto/TransactionDto.java
@@ -0,0 +1,40 @@
+package com.example.account.dto;
+
+import com.example.account.domain.Account;
+import com.example.account.domain.Transaction;
+import com.example.account.type.TransactionResultType;
+import com.example.account.type.TransactionType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TransactionDto {
+    private String accountNumber;
+    private TransactionType transactionType;
+    private TransactionResultType transactionResultType;
+    private Account account;
+    private Long amount;
+    private Long balanceSnapShot;
+    private String transactionId;
+    private LocalDateTime transactedAt;
+
+    public static TransactionDto fromEntity(Transaction transaction) {
+        return TransactionDto.builder()
+                .accountNumber(transaction.getAccount().getAccountNumber())
+                .transactionType(transaction.getTransactionType())
+                .transactionResultType(transaction.getTransactionResultType())
+                .amount(transaction.getAmount())
+                .balanceSnapShot(transaction.getBalanceSnapShot())
+                .transactionId(transaction.getTransactionId())
+                .transactedAt(transaction.getTransactedAt())
+                .build();
+    }
+}
